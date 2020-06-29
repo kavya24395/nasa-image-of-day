e328a8e07914bba61678d81dc7f3e2db2ab58c9f
Kavya P S <kavyaps@Kavyas-MacBook-Pro.local>
2020-06-29 22:41:39 +0530

    Adding APOD repository data fetching functionality

--- app/src/main/java/com/kavya/nasa_photooftheday/repository/Repository.kt
+++ app/src/main/java/com/kavya/nasa_photooftheday/repository/Repository.kt
@@ -1,5 +1,25 @@
 package com.kavya.nasa_photooftheday.repository
 
+import com.kavya.nasa_photooftheday.repository.network.ApodApi
+import com.kavya.nasa_photooftheday.repository.network.ApodResponse
+import com.kavya.nasa_photooftheday.util.NetworkUtils
+import retrofit2.Call
+
 /**
+ * Repository of APOD data
+ *
+ *
  * Created by Kavya P S on 29/06/20.
  */
+object Repository {
+
+    fun fetchApodForCurrentDay(): Call<ApodResponse> {
+        return NetworkUtils.getNetworkHook(ApodApi::class.java)
+            .fetchApodForCurrentDay()
+    }
+
+    fun fetchApodForDate(date: String): Call<ApodResponse> {
+        return NetworkUtils.getNetworkHook(ApodApi::class.java)
+            .fetchApodForDate(date)
+    }
+}
